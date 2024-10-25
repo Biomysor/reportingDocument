44ec8cf140c1887eb20d49ae5baa39a6bfe67fc9
Biomysor <160412094+Biomysor@users.noreply.github.com>
2024-10-25 11:56:38 +0300

    3лаба

--- script.js
+++ script.js
@@ -3,7 +3,8 @@ const lab1Details = document.getElementById('lab1-details');
 const contentDiv = document.getElementById('content'); 
 const lab2Button = document.getElementById('lab2');
 const lab2Details = document.getElementById('lab2-details');
-
+const lab3Button = document.getElementById('lab3');
+const lab3Details = document.getElementById('lab3-details');
 // Функція для відображення тексту та посилання
 function displayContent(text, link) {
     contentDiv.innerHTML = ''; // Очищаємо вміст
@@ -27,7 +28,8 @@ function displayContent(text, link) {
 // Обробник для лабораторної роботи 1
 lab1Button.addEventListener('click', function (event) {
     event.preventDefault(); 
-    lab2Details.classList.add('hidden'); // Ховаємо деталі 2 лабораторної роботи
+    lab2Details.classList.add('hidden');
+    lab3Details.classList.add('hidden'); // Ховаємо деталі 2 лабораторної роботи
     lab1Details.classList.toggle('hidden'); // Перемикаємо видимість деталей 1 лабораторної роботи
     contentDiv.innerHTML = ''; // Очищаємо вміст
 });
@@ -35,11 +37,19 @@ lab1Button.addEventListener('click', function (event) {
 // Обробник для лабораторної роботи 2
 lab2Button.addEventListener('click', function (event) {
     event.preventDefault(); 
-    lab1Details.classList.add('hidden'); // Ховаємо деталі 1 лабораторної роботи
+    lab1Details.classList.add('hidden');
+    lab3Details.classList.add('hidden'); // Ховаємо деталі 1 лабораторної роботи
     lab2Details.classList.toggle('hidden'); // Перемикаємо видимість деталей 2 лабораторної роботи
     contentDiv.innerHTML = ''; // Очищаємо вміст
 });
 
+lab3Button.addEventListener('click', function (event) {
+    event.preventDefault(); 
+    lab1Details.classList.add('hidden');
+    lab2Details.classList.add('hidden'); // Ховаємо деталі 2 лабораторної роботи
+    lab3Details.classList.toggle('hidden'); // Перемикаємо видимість деталей 1 лабораторної роботи
+    contentDiv.innerHTML = ''; // Очищаємо вміст
+});
 // Обробник для кнопок в деталях лабораторної роботи 1
 lab1Details.addEventListener('click', function (event) {
     if (event.target.tagName === 'BUTTON') {
@@ -54,6 +64,13 @@ lab2Details.addEventListener('click', function (event) {
     if (event.target.tagName === 'BUTTON') {
         const text = event.target.getAttribute('data-text'); 
         const link = event.target.getAttribute('data-link'); // Отримуємо посилання з кнопки
+        displayContent(text, link);
+    }
+});
+lab3Details.addEventListener('click', function (event) {
+    if (event.target.tagName === 'BUTTON') {
+        const text = event.target.getAttribute('data-text'); 
+        const link = event.target.getAttribute('data-link'); // Постійне посилання для лаби 1
         displayContent(text, link);
     }
 });
